f0b204b39f9fbf9d3c312eadd62d8be44b5312b2
unknown <danaleighvyver@gmail.com>
2026-06-14 17:20:43 +0200

    Updated README and font sizing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <div>
-            <p id="footer">
+            <footer>
                 Coded in Visual Studio Code by me. Built with Next.js.
                 Hosted on GitHub Pages.
-            </p>
+            </footer>
         </div>
 
     );
